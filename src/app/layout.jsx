4bf680df5ea86fcbbe0981fe00dd174c151007fb
stevenfvg <stevenvalladaresgiron@gmail.com>
2023-10-29 11:34:38 -0300

    style: :lipstick: google fonts added
    encode sans fonts added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,24 @@
+import { Encode_Sans } from 'next/font/google';
 import { Providers } from '../providers/providers';
 import '../styles/globals.css';
 
+// HTML metadata
 export const metadata = {
   title: 'Documentation Prework Setup',
 };
 
+// Font Settings
+const encodeSans = Encode_Sans({
+  weight: ['300', '400', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
-      <body>
+      <body className={encodeSans.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
